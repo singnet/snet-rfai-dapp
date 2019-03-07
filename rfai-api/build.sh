b9e147e4d93096ffff3cf0e208d44e715bbb1ecd
prashantramangupta <you@example.com>
2019-03-07 11:12:43 +0530

    api to read & write content to ipfs

---
+++ rfai-api/build.sh
@@ -0,0 +1,2 @@
+rm rfai.zip
+zip -r rfai.zip * -x \*venv\*
---
+++ rfai-api/common/constant.py
@@ -0,0 +1,14 @@
+IPFS_URL = {
+    'url': 'ipfs.singularitynet.io',
+    'port': '80',
+    'protocol': 'http'
+}
+SLACK_HOOK = {
+    'hostname' : 'https://hooks.slack.com',
+    'port': 443,
+    'path': '/services/T996H7VS8/BFF77DU6A/tnsnqohmdaoWRvRqSEH7MNhr',
+    'method': 'POST',
+    'headers': {
+        'Content-Type': 'application/json'
+    }
+}
