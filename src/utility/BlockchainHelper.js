f579897c639c3a29173ce219518b17b48c9dfd48
Sridhar Babu Kolapalli <ksridharbabuus@gmail.com>
2019-11-12 13:00:54 +0530

    Blockchain Interface functions are added2

--- src/utility/BlockchainHelper.js
+++ src/utility/BlockchainHelper.js
@@ -212,6 +212,133 @@ export const claimBackRequest = (metamaskDetails, requestId) => {
   });
 };
 
+export const approveRequest = (metamaskDetails, requestId, endSubmission, endEvaluation, newExpiration) => {
+  const rfaiContractAddress = getRFAIContractAddress();
+  const accountAddress = metamaskDetails.account;
+
+  const ethereum = window.ethereum;
+  window.web3 = new window.Web3(ethereum);
+
+  const rfaiInstance = window.web3.eth.contract(rfai.abi).at(rfaiContractAddress);
+
+  return new Promise(async (resolve, reject) => {
+    await rfaiInstance.approveRequest(
+      requestId,
+      endSubmission,
+      endEvaluation,
+      newExpiration,
+      { from: accountAddress },
+      (err, hash) => {
+        if (err) {
+          reject(hash);
+        }
+        resolve(hash);
+      }
+    );
+  });
+};
+
+// Can by Owner or by Active Foundation Member only
+export const rejectRequest = (metamaskDetails, requestId) => {
+  const rfaiContractAddress = getRFAIContractAddress();
+  const accountAddress = metamaskDetails.account;
+
+  const ethereum = window.ethereum;
+  window.web3 = new window.Web3(ethereum);
+
+  const rfaiInstance = window.web3.eth.contract(rfai.abi).at(rfaiContractAddress);
+
+  return new Promise(async (resolve, reject) => {
+    await rfaiInstance.rejectRequest(requestId, { from: accountAddress }, (err, hash) => {
+      if (err) {
+        reject(hash);
+      }
+      resolve(hash);
+    });
+  });
+};
+
+// Can be called by Owner/Foundation Member or Requester when it is in Open State
+export const closeRequest = (metamaskDetails, requestId) => {
+  const rfaiContractAddress = getRFAIContractAddress();
+  const accountAddress = metamaskDetails.account;
+
+  const ethereum = window.ethereum;
+  window.web3 = new window.Web3(ethereum);
+
+  const rfaiInstance = window.web3.eth.contract(rfai.abi).at(rfaiContractAddress);
+
+  return new Promise(async (resolve, reject) => {
+    await rfaiInstance.closeRequest(requestId, { from: accountAddress }, (err, hash) => {
+      if (err) {
+        reject(hash);
+      }
+      resolve(hash);
+    });
+  });
+};
+
+// Can be done only owner or other member with role=1
+// Role can be 0 or 1 & active will be true/false
+export const addOrUpdateFoundationMembers = (metamaskDetails, member, role, active) => {
+  const rfaiContractAddress = getRFAIContractAddress();
+  const accountAddress = metamaskDetails.account;
+
+  const ethereum = window.ethereum;
+  window.web3 = new window.Web3(ethereum);
+
+  const rfaiInstance = window.web3.eth.contract(rfai.abi).at(rfaiContractAddress);
+
+  return new Promise(async (resolve, reject) => {
+    await rfaiInstance.addOrUpdateFoundationMembers(member, role, active, { from: accountAddress }, (err, hash) => {
+      if (err) {
+        reject(hash);
+      }
+      resolve(hash);
+    });
+  });
+};
+
+// Can be done only by owner
+export const updateOwner = (metamaskDetails, newOwner) => {
+  const rfaiContractAddress = getRFAIContractAddress();
+  const accountAddress = metamaskDetails.account;
+
+  const ethereum = window.ethereum;
+  window.web3 = new window.Web3(ethereum);
+
+  const rfaiInstance = window.web3.eth.contract(rfai.abi).at(rfaiContractAddress);
+
+  return new Promise(async (resolve, reject) => {
+    await rfaiInstance.updateOwner(newOwner, { from: accountAddress }, (err, hash) => {
+      if (err) {
+        reject(hash);
+      }
+      resolve(hash);
+    });
+  });
+};
+
+// Can be done by only owner
+export const updateConfigLimits = (metamaskDetails, minStake, maxStakers) => {
+  const rfaiContractAddress = getRFAIContractAddress();
+  const accountAddress = metamaskDetails.account;
+
+  const ethereum = window.ethereum;
+  window.web3 = new window.Web3(ethereum);
+
+  const rfaiInstance = window.web3.eth.contract(rfai.abi).at(rfaiContractAddress);
+
+  return new Promise(async (resolve, reject) => {
+    await rfaiInstance.updateLimits(minStake, maxStakers, { from: accountAddress }, (err, hash) => {
+      if (err) {
+        reject(hash);
+      }
+      resolve(hash);
+    });
+  });
+};
+
 export const getBlockNumber = () => {
   const ethereum = window.ethereum;
   window.web3 = new window.Web3(ethereum);
