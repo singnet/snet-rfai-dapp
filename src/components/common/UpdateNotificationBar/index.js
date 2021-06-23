331d94fb26cf07bb776e77d9059405edc5b49ed9
Rajeev <scraptorajeev@gmail.com>
2021-06-23 11:07:39 +0530

    FIX: Broken margin height on Landing page

--- src/components/common/UpdateNotificationBar/index.js
+++ src/components/common/UpdateNotificationBar/index.js
@@ -13,12 +13,11 @@ const UpdateNotificationBar = ({ classes, showNotification, onCloseClick }) => {
         <img src={AnnoucementIcon} alt="Announcment" />
       </div>
       <div className={classes.content}>
-        <p>Dear SingularityNET supporter,</p>
         <p>
-          We are in the process of making some changes to RFAI portal and community-driven AI development support in
-          general. To avoid unnecessary spending of AGIX or transaction costs, we have disabled new entries to the
-          portal, until further notice. Stay tuned and follow our social media channels to be informed about the many
-          exciting developments to come!
+          Dear SingularityNET supporter, We are in the process of making some changes to RFAI portal and
+          community-driven AI development support in general. To avoid unnecessary spending of AGIX or transaction
+          costs, we have disabled new entries to the portal, until further notice. Stay tuned and follow our social
+          media channels to be informed about the many exciting developments to come!
         </p>
       </div>
       {/* <div>
